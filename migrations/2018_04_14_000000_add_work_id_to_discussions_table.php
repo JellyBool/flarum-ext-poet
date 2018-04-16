940a1cdf461e072ee7a551bea80a07cef8cb0a7d
JellyBool <jellybool@outlook.com>
2018-04-16 13:59:38 +0800

    add migration

--- migrations/2018_04_14_000000_add_work_id_to_discussions_table.php
+++ migrations/2018_04_14_000000_add_work_id_to_discussions_table.php
@@ -1,6 +1,16 @@
 <?php
-use Flarum\Database\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
 
-return Migration::addColumns('discussions', [
-    'work_id' => ['string','nullable']
-]);
+return [
+    'up' => function (Builder $schema) {
+        $schema->table('discussions', function (Blueprint $table) {
+            $table->string('wokr_id')->nullable();
+        });
+    },
+    'down' => function (Builder $schema) {
+        $schema->table('discussions', function (Blueprint $table) {
+            $table->dropColumn(['wokr_id']);
+        });
+    }
+];
