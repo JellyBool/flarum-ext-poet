f3750d0dee6e863c151ac602bf6f1f154076c37c
JellyBool <jellybool@outlook.com>
2018-04-16 13:48:31 +0800

    add migration

--- migrations/2018_04_15_000000_change_work_id_to_nullable.php
+++ migrations/2018_04_15_000000_change_work_id_to_nullable.php
@@ -4,12 +4,12 @@ use Illuminate\Database\Schema\Builder;
 return [
     'up' => function (Builder $schema) {
         $schema->table('discussions', function (Blueprint $table) {
-            $table->string('work_id')->nullable()->change();
+            $table->string('work_id')->defalut('')->change();
         });
     },
     'down' => function (Builder $schema) {
         $schema->table('discussions', function (Blueprint $table) {
-            $table->string('work_id')->nullable()->change();
+            $table->string('work_id')->defalut('')->change();
         });
     }
 ];
